68b36e7d0508ca986beab0419f564b0bbdfa421d
droan <andro.rezic@gmail.com>
2013-04-02 18:25:15 +0200

    fixed landing form text

--- landing/forms.py
+++ landing/forms.py
@@ -2,4 +2,4 @@
 from django import forms
 
 class SkipForm(forms.Form):
-    skip = forms.BooleanField(required=False, label=u'Preskoči ovu stranicu prilikom slijedećeg posjeta.')
+    skip = forms.BooleanField(required=False, label=u'Preskoči ovu stranicu prilikom sljedećeg posjeta.')
